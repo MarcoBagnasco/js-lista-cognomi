e5863aec80e548e0246caa88bd217c92cb88659f
MarcoBagnasco <bagna.hbk@gmail.com>
2021-03-24 13:33:46 +0100

    add task 1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-console.log('hello');
+/*
+LISTA COGNOMI
+    1. chiedi all’utente il cognome
+    2. inseriscilo in un array con altri cognomi
+    3. stampa la lista ordinata alfabeticamente
+    4. scrivi anche la posizione "umana"
+ */
+
+var lastNameList = ['rossi', 'duzioni', 'bagnasco', 'giorgini', 'balsano', 'perlari'];
+console.log(lastNameList); //test
+
+var userLastName = prompt('Insert lastname');
+console.log(userLastName); //test
+
+lastNameList.push(userLastName);
+console.log(lastNameList); //test
